001320498cfe0ef1cf2dd9061da70a086252c0ea
koppepam <rise.yoneyama@gmail.com>
2026-07-22 20:26:30 +0900

    eslint を設定・修正対応

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,7 +103,7 @@ export default function TopPage() {
                 <div className="p-4 sm:flex items-center px-2">
                   <div className="pl-3 pb-2 sm:pl-2 sm:pb-0">
                     <div className="pt-0.5">
-                      <img src={process.env.NEXT_PUBLIC_GITHUB_PAGES ? "/faq/images/cc.png" : "/images/cc.png"} width={88} height={31} />
+                      <img src={process.env.NEXT_PUBLIC_GITHUB_PAGES ? "/faq/images/cc.png" : "/images/cc.png"} width={88} height={31} alt="Creative Commons" />
                     </div>
                   </div>
                   <div className="ml-3 sm:w-72 leading-5 ">
@@ -119,7 +119,7 @@ export default function TopPage() {
                 <div className="p-4 sm:flex items-center px-2">
                   <div className="pl-3 pb-2 sm:pl-2 sm:pb-0">
                     <div className="icon">
-                      <img src={process.env.NEXT_PUBLIC_GITHUB_PAGES ? "/faq/images/github.png" : "/images/github.png"} width={100} height={100} />
+                      <img src={process.env.NEXT_PUBLIC_GITHUB_PAGES ? "/faq/images/github.png" : "/images/github.png"} width={100} height={100} alt="GitHub" />
                     </div>
                   </div>
                   <div className=" ml-3 sm:w-96 leading-5 ">
